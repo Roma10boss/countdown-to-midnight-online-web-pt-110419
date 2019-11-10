d9308a5e07c0191c0b66c1908304c945b74e95a5
Romario Gustave <romagustave10@gmail.com>
2019-11-10 00:34:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,14 @@
 def inte 
   #write your code here
+  puts " Enter your seconds"
+  num = gets.chomp 
+  end
 
 def countdown
-  
+  counter = num
+  while counter < num
+  puts "#{num} seconds"
+  counter -=1
+  if counter= 0 
+    puts "Happy new year"
 end
